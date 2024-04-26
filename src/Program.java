0cd5fa6beb7d6a655ef6ac2d6ce2c0c4949c7bdc
jowynd <jowynd12@gmail.com>
2024-04-26 19:21:40 -0300

    starting to implement board and piece

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,10 @@
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+        Board board = new Board(8, 8);
+        System.out.println(board);
     }
 }
