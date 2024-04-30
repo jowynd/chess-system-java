41405c63e713565e2abec6aafc72a7dd8f3efc23
jowynd <jowynd12@gmail.com>
2024-04-30 17:43:11 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -28,21 +27,18 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('a', 2, new King(board, Color.WHITE));
-        placeNewPiece('b', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('b', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new King(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('f', 1, new King(board, Color.WHITE));
-        placeNewPiece('f', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('g', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('g', 2, new King(board, Color.WHITE));
-        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('h', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
