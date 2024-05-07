731e5120dcaf92210a8c537cf5fd71ca6d0e0da3
jowynd <jowynd12@gmail.com>
2024-05-07 19:36:05 -0300

    handling captured pieces

--- src/chess/UI.java
+++ src/chess/UI.java
@@ -1,6 +1,9 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
@@ -29,13 +32,13 @@ public class UI {
         return new ChessPosition(column, row);
     }
 
-    public static void printMatch (ChessMatch chessMatch) {
+    public static void printMatch (ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
-        System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println();
+        printCapturedPieces(captured);
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting for player " + chessMatch.getCurrentPlayer());
-
-
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
@@ -61,6 +64,22 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        System.out.println("Captured pieces");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
+
     }
 }
 
